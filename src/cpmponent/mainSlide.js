4e7c0c80d16921cb4e25d31e9047bcf9a8e6e78a
mahde13 <musavemahdy>
2024-03-20 15:16:58 -0800

    add more option to  mainSlides

--- src/cpmponent/mainSlide.js
+++ src/cpmponent/mainSlide.js
@@ -11,17 +11,26 @@ const MainSlide = () => {
       id: 1,
       src: MainBanner1,
       alt: "First Slide",
+      title: "yesterday",
+      description:
+        "Use data attributes to easily control the position of the carousel. data-slide accepts the keywords prev or next, which alters the slide position relative to its current position. Alternatively, use data-slide-to to pass a raw slide index to the carousel data-slide-to=2, which shifts the slide position to a particular index beginning with 0",
     },
     {
       id: 2,
       src: MainBanner2,
       alt: "second Slide",
+      title: "Today",
+      description:
+        "Options can be passed via data attributes or JavaScript. For data attributes, append the option name to data-, as in data-interval=",
     },
 
     {
       id: 3,
       src: MainBanner3,
       alt: "third Slide",
+      title: "tomarow",
+      description:
+        "Examples and usage guidelines for form control styles, layout options, and custom components for creating a wide variety of forms.",
     },
   ];
 
@@ -33,12 +42,22 @@ const MainSlide = () => {
             // return console.log(imageSlide);
             return (
               <Carousel.Item key={imageSlide.id}>
-                <Link to="/">
-                  <img
-                    src={imageSlide.src}
-                    alt={imageSlide.alt}
-                    className="img-fluid w-100"
-                  />
+                <Link to="/" className="text-decoration-none ">
+                  <div className="card-head row ">
+                    <div className="head-title">
+                      <h2>{imageSlide.title}</h2>
+                    </div>
+                    <div className="card-body">
+                      <img
+                        src={imageSlide.src}
+                        alt={imageSlide.alt}
+                        className="img-fluid w-100"
+                      />
+                    </div>
+                    <div className="card-footer ps-5 py-5">
+                      <p>{imageSlide.description}</p>
+                    </div>
+                  </div>
                 </Link>
               </Carousel.Item>
             );
