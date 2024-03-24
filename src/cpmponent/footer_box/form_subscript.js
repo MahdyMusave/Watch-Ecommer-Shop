4c9fbe3437bd232c727a5e22d98990cb59a327ad
mahde13 <musavemahdy>
2024-03-23 21:09:31 -0800

    add subScript eamil

---
+++ src/cpmponent/footer_box/form_subscript.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FormSubscript = () => {
+  return (
+    <>
+      <div className="subscript-form">
+        <form method="post">
+          <input type="text" name="email" placeholder="Your email" />
+          <button className="">Subscript</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default FormSubscript;
